799587bcb8bac46762e07a94572a60c3e892f347
OnlyZalll <faizal.fajartn@gmail.com>
2025-05-18 10:02:00 +0700

    .

--- pyrogram/types/messages_and_media/__init__.py
+++ pyrogram/types/messages_and_media/__init__.py
@@ -23,14 +23,15 @@ from .contact import Contact
 from .dice import Dice
 from .document import Document
 from .forum_topic import ForumTopic
-from .forum_topic_created import ForumTopicCreated
 from .forum_topic_closed import ForumTopicClosed
-from .forum_topic_reopened import ForumTopicReopened
+from .forum_topic_created import ForumTopicCreated
 from .forum_topic_edited import ForumTopicEdited
+from .forum_topic_reopened import ForumTopicReopened
+from .game import Game
 from .general_forum_topic_hidden import GeneralTopicHidden
 from .general_forum_topic_unhidden import GeneralTopicUnhidden
-from .game import Game
 from .giveaway import Giveaway
+from .link_preview_options import LinkPreviewOptions
 from .location import Location
 from .message import Message
 from .message_entity import MessageEntity
@@ -40,16 +41,19 @@ from .message_origin_chat import MessageOriginChat
 from .message_origin_hidden_user import MessageOriginHiddenUser
 from .message_origin_import import MessageOriginImport
 from .message_origin_user import MessageOriginUser
+from .message_reactions import MessageReactions
+from .message_story import MessageStory
+from .my_boost import MyBoost
 from .photo import Photo
 from .poll import Poll
 from .poll_option import PollOption
 from .reaction import Reaction
 from .sticker import Sticker
-from .stripped_thumbnail import StrippedThumbnail
 from .story import Story
 from .story_deleted import StoryDeleted
 from .story_skipped import StorySkipped
 from .story_views import StoryViews
+from .stripped_thumbnail import StrippedThumbnail
 from .thumbnail import Thumbnail
 from .venue import Venue
 from .video import Video
@@ -57,15 +61,51 @@ from .video_note import VideoNote
 from .voice import Voice
 from .web_app_data import WebAppData
 from .web_page import WebPage
-from .message_reactions import MessageReactions
-from .message_story import MessageStory
-from .my_boost import MyBoost
 
 __all__ = [
-    "Animation", "Audio", "BoostsStatus", "Contact", "Document", "ForumTopic", "MessageOrigin", "ForumTopicCreated",
-    "ForumTopicClosed", "ForumTopicReopened", "ForumTopicEdited", "GeneralTopicHidden",
-    "GeneralTopicUnhidden", "Game", "Giveaway", "Location", "Message", "MessageEntity", "Photo", "Thumbnail",
-    "StrippedThumbnail", "Story", "StoryDeleted", "StorySkipped", "StoryViews", "Poll", "PollOption", "Sticker",
-    "Venue", "Video", "VideoNote", "Voice", "WebPage", "Dice", "Reaction", "WebAppData",
-    "MessageReactions", "MessageStory", "MyBoost", "MessageOriginUser", "MessageOriginImport", "MessageOriginHiddenUser", "MessageOriginChat", "MessageOriginChannel"
+    "Animation",
+    "Audio",
+    "BoostsStatus",
+    "Contact",
+    "Dice",
+    "Document",
+    "ForumTopic",
+    "ForumTopicClosed",
+    "ForumTopicCreated",
+    "ForumTopicEdited",
+    "ForumTopicReopened",
+    "Game",
+    "GeneralTopicHidden",
+    "GeneralTopicUnhidden",
+    "Giveaway",
+    "LinkPreviewOptions",
+    "Location",
+    "Message",
+    "MessageEntity",
+    "MessageOrigin",
+    "MessageOriginChannel",
+    "MessageOriginChat",
+    "MessageOriginHiddenUser",
+    "MessageOriginImport",
+    "MessageOriginUser",
+    "MessageReactions",
+    "MessageStory",
+    "MyBoost",
+    "Photo",
+    "Poll",
+    "PollOption",
+    "Reaction",
+    "Sticker",
+    "Story",
+    "StoryDeleted",
+    "StorySkipped",
+    "StoryViews",
+    "StrippedThumbnail",
+    "Thumbnail",
+    "Venue",
+    "Video",
+    "VideoNote",
+    "Voice",
+    "WebAppData",
+    "WebPage"
 ]
