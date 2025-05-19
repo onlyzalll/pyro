65ccf5996b61129169833feb1ad3269b5505de03
OnlyZalll <faizal.fajartn@gmail.com>
2025-05-19 22:45:33 +0700

    .

--- pyrogram/methods/advanced/resolve_peer.py
+++ pyrogram/methods/advanced/resolve_peer.py
@@ -21,8 +21,7 @@ import re
 from typing import Union
 
 import pyrogram
-from pyrogram import raw
-from pyrogram import utils
+from pyrogram import raw, utils
 from pyrogram.errors import PeerIdInvalid
 
 log = logging.getLogger(__name__)
@@ -30,10 +29,10 @@ log = logging.getLogger(__name__)
 
 class ResolvePeer:
     async def resolve_peer(
-        self: "pyrogram.Client",
-        peer_id: Union[int, str]
+        self: "pyrogram.Client", peer_id: Union[int, str]
     ) -> Union[raw.base.InputPeer, raw.base.InputUser, raw.base.InputChannel]:
-        """Get the InputPeer of a known peer id. Useful whenever an InputPeer type is required.
+        """Get the InputPeer of a known peer id.
+        Useful whenever an InputPeer type is required.
 
         .. note::
 
@@ -46,10 +45,10 @@ class ResolvePeer:
         Parameters:
             peer_id (``int`` | ``str``):
                 The peer id you want to extract the InputPeer from.
-                Can be a direct id (int), a username (str), a link (str) or a phone number (str).
+                Can be a direct id (int), a username (str) or a phone number (str) or *t.me/<username>* link.
 
         Returns:
-            :obj:`~pyrogram.raw.base.InputPeer`: On success, the resolved peer id is returned in form of an InputPeer object.
+            ``InputPeer``: On success, the resolved peer id is returned in form of an InputPeer object.
 
         Raises:
             KeyError: In case the peer doesn't exist in the internal database.
@@ -57,22 +56,15 @@ class ResolvePeer:
         if not self.is_connected:
             raise ConnectionError("Client has not been started yet")
 
-        if peer_id in ("self", "me"):
-            return raw.types.InputPeerSelf()
-
         try:
             return await self.storage.get_peer_by_id(peer_id)
         except KeyError:
             if isinstance(peer_id, str):
-                match = re.match(r"^(?:https?://)?(?:www\.)?(?:t(?:elegram)?\.(?:org|me|dog)/(?:c/)?)([\w]+)(?:.+)?$", peer_id.lower())
+                if peer_id in ("self", "me"):
+                    return raw.types.InputPeerSelf()
 
-                if match:
-                    try:
-                        peer_id = utils.get_channel_id(int(match.group(1)))
-                    except ValueError:
-                        peer_id = match.group(1)
-                else:
-                    peer_id = re.sub(r"[@+\s]", "", peer_id.lower())
+                peer_id = re.sub(r"[@+\s]", "", peer_id.lower())
+                peer_id = re.sub(r"https://t.me/", "", peer_id.lower())
 
                 try:
                     int(peer_id)
@@ -80,16 +72,9 @@ class ResolvePeer:
                     try:
                         return await self.storage.get_peer_by_username(peer_id)
                     except KeyError:
-                        r = await self.invoke(
-                            raw.functions.contacts.ResolveUsername(
-                                username=peer_id
-                            )
+                        await self.invoke(
+                            raw.functions.contacts.ResolveUsername(username=peer_id)
                         )
-
-                        if isinstance(r.peer, raw.types.PeerUser):
-                            return await self.storage.get_peer_by_id(r.peer.user_id)
-                        elif isinstance(r.peer, raw.types.PeerChannel):
-                            return await self.storage.get_peer_by_id(utils.get_channel_id(r.peer.channel_id))
 
                         return await self.storage.get_peer_by_username(peer_id)
                 else:
@@ -104,32 +89,23 @@ class ResolvePeer:
                 await self.fetch_peers(
                     await self.invoke(
                         raw.functions.users.GetUsers(
-                            id=[
-                                raw.types.InputUser(
-                                    user_id=peer_id,
-                                    access_hash=0
-                                )
-                            ]
+                            id=[raw.types.InputUser(user_id=peer_id, access_hash=0)]
                         )
                     )
                 )
             elif peer_type == "chat":
-                await self.invoke(
-                    raw.functions.messages.GetChats(
-                        id=[-peer_id]
-                    )
-                )
+                await self.invoke(raw.functions.messages.GetChats(id=[-peer_id]))
             else:
                 await self.invoke(
                     raw.functions.channels.GetChannels(
                         id=[
                             raw.types.InputChannel(
-                                channel_id=utils.get_channel_id(peer_id),
-                                access_hash=0
+                                channel_id=utils.get_channel_id(peer_id), access_hash=0
                             )
                         ]
                     )
                 )
+
             try:
                 return await self.storage.get_peer_by_id(peer_id)
             except KeyError:
--- pyrogram/methods/advanced/save_file.py
+++ pyrogram/methods/advanced/save_file.py
@@ -25,11 +25,10 @@ import math
 import os
 from hashlib import md5
 from pathlib import PurePath
-from typing import Union, BinaryIO, Callable
+from typing import BinaryIO, Callable, Union
 
 import pyrogram
-from pyrogram import StopTransmission
-from pyrogram import raw
+from pyrogram import StopTransmission, raw
 from pyrogram.session import Session
 
 log = logging.getLogger(__name__)
@@ -42,7 +41,7 @@ class SaveFile:
         file_id: int = None,
         file_part: int = 0,
         progress: Callable = None,
-        progress_args: tuple = ()
+        progress_args: tuple = (),
     ):
         """Upload a file onto Telegram servers, without actually sending the message to anyone.
         Useful whenever an InputFile type is required.
@@ -117,7 +116,9 @@ class SaveFile:
             elif isinstance(path, io.IOBase):
                 fp = path
             else:
-                raise ValueError("Invalid file. Expected a file path as string or a binary (not text) file pointer")
+                raise ValueError(
+                    "Invalid file. Expected a file path as string or a binary (not text) file pointer"
+                )
 
             file_name = getattr(fp, "name", "file.jpg")
 
@@ -128,13 +129,12 @@ class SaveFile:
             if file_size == 0:
                 raise ValueError("File size equals to 0 B")
 
-            if self.me and self.me.is_premium:
-                file_size_limit_mib = 4000
-            else:
-                file_size_limit_mib = 2000
+            file_size_limit_mib = 4000 if self.me.is_premium else 2000
 
             if file_size > file_size_limit_mib * 1024 * 1024:
-                raise ValueError(f"Can't upload files bigger than {file_size_limit_mib} MiB")
+                raise ValueError(
+                    f"Can't upload files bigger than {file_size_limit_mib} MiB"
+                )
 
             file_total_parts = int(math.ceil(file_size / part_size))
             is_big = file_size > 10 * 1024 * 1024
@@ -142,20 +142,21 @@ class SaveFile:
             is_missing_part = file_id is not None
             file_id = file_id or self.rnd_id()
             md5_sum = md5() if not is_big and not is_missing_part else None
-            dc_id = await self.storage.dc_id()
-
-            session = self.media_sessions.get(dc_id)
-            if not session:
-                session = self.media_sessions[dc_id] = Session(
-                    self, dc_id, await self.storage.auth_key(),
-                    await self.storage.test_mode(), is_media=True
-                )
-                await session.start()
-
-            workers = [self.loop.create_task(worker(session)) for _ in range(workers_count)]
+            session = Session(
+                self,
+                await self.storage.dc_id(),
+                await self.storage.auth_key(),
+                await self.storage.test_mode(),
+                is_media=True,
+            )
+            workers = [
+                self.loop.create_task(worker(session)) for _ in range(workers_count)
+            ]
             queue = asyncio.Queue(1)
 
             try:
+                await session.start()
+
                 fp.seek(part_size * file_part)
 
                 while True:
@@ -163,7 +164,9 @@ class SaveFile:
 
                     if not chunk:
                         if not is_big and not is_missing_part:
-                            md5_sum = "".join([hex(i)[2:].zfill(2) for i in md5_sum.digest()])
+                            md5_sum = "".join(
+                                [hex(i)[2:].zfill(2) for i in md5_sum.digest()]
+                            )
                         break
 
                     if is_big:
@@ -171,13 +174,11 @@ class SaveFile:
                             file_id=file_id,
                             file_part=file_part,
                             file_total_parts=file_total_parts,
-                            bytes=chunk
+                            bytes=chunk,
                         )
                     else:
                         rpc = raw.functions.upload.SaveFilePart(
-                            file_id=file_id,
-                            file_part=file_part,
-                            bytes=chunk
+                            file_id=file_id, file_part=file_part, bytes=chunk
                         )
 
                     await queue.put(rpc)
@@ -195,7 +196,7 @@ class SaveFile:
                             progress,
                             min(file_part * part_size, file_size),
                             file_size,
-                            *progress_args
+                            *progress_args,
                         )
 
                         if inspect.iscoroutinefunction(progress):
@@ -212,20 +213,21 @@ class SaveFile:
                         id=file_id,
                         parts=file_total_parts,
                         name=file_name,
-
                     )
                 else:
                     return raw.types.InputFile(
                         id=file_id,
                         parts=file_total_parts,
                         name=file_name,
-                        md5_checksum=md5_sum
+                        md5_checksum=md5_sum,
                     )
             finally:
                 for _ in workers:
                     await queue.put(None)
 
                 await asyncio.gather(*workers)
+
+                await session.stop()
 
                 if isinstance(path, (str, PurePath)):
                     fp.close()
