959cf8543eaeeeaf56dcf7bc2b915cc484110e49
OnlyZalll <faizal.fajartn@gmail.com>
2025-05-20 16:58:19 +0700

    .

--- pyrogram/methods/business/__init__.py
+++ pyrogram/methods/business/__init__.py
@@ -16,38 +16,16 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with Pyrogram.  If not, see <http://www.gnu.org/licenses/>.
 
-from .advanced import Advanced
-from .auth import Auth
-from .bots import Bots
-from .business import Business
-from .chats import Chats
-from .contacts import Contacts
-from .decorators import Decorators
-from .invite_links import InviteLinks
-from .messages import Messages
-from .password import Password
-from .premium import Premium
-from .phone import Phone
-from .users import Users
-from .stories import Stories
-from .utilities import Utilities
+from .delete_business_messages import DeleteBusinessMessages
+from .get_business_account_gifts import GetBusinessAccountGifts
+from .get_business_account_star_balance import GetBusinessAccountStarBalance
+from .get_business_connection import GetBusinessConnection
 
 
-class Methods(
-    Advanced,
-    Auth,
-    Bots,
-    Business,
-    Contacts,
-    Password,
-    Premium,
-    Phone,
-    Chats,
-    Users,
-    Stories,
-    Messages,
-    Decorators,
-    Utilities,
-    InviteLinks,
+class Business(
+    DeleteBusinessMessages,
+    GetBusinessAccountGifts,
+    GetBusinessAccountStarBalance,
+    GetBusinessConnection
 ):
     pass
