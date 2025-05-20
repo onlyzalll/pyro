1d5fe6cfec0f8f81e34fe6bb866c5b54243e3336
OnlyZalll <faizal.fajartn@gmail.com>
2025-05-20 18:36:37 +0700

    .

--- pyrogram/enums/__init__.py
+++ pyrogram/enums/__init__.py
@@ -33,13 +33,17 @@ from .messages_filter import MessagesFilter
 from .next_code_type import NextCodeType
 from .paid_reaction_privacy import PaidReactionPrivacy
 from .parse_mode import ParseMode
+from .phone_call_discard_reason import PhoneCallDiscardReason
 from .poll_type import PollType
+from .privacy_key import PrivacyKey
+from .privacy_rule_type import PrivacyRuleType
 from .profile_color import ProfileColor
 from .reply_color import ReplyColor
 from .sent_code_type import SentCodeType
 from .gift_attribute_type import GiftAttributeType
 from .gift_for_resale_order import GiftForResaleOrder
 from .media_area_type import MediaAreaType
+from .stories_privacy_rules import StoriesPrivacyRules
 from .user_status import UserStatus
 
 __all__ = [
@@ -60,12 +64,16 @@ __all__ = [
     'NextCodeType',
     'PaidReactionPrivacy',
     'ParseMode',
+    'PhoneCallDiscardReason',
     'PollType',
+    'PrivacyKey',
+    'PrivacyRuleType',
     'ProfileColor',
     'ReplyColor',
     'SentCodeType',
     'GiftAttributeType',
     'GiftForResaleOrder',
     'MediaAreaType',
+    'StoriesPrivacyRules',
     'UserStatus'
 ]
