d368fc09b81cd5c95ebc2a094eb30462983b7013
OnlyZalll <faizal.fajartn@gmail.com>
2025-05-21 20:49:25 +0700

    tes

--- pyrogram/client.py
+++ pyrogram/client.py
@@ -296,8 +296,7 @@ class Client(Methods):
         # after some idle time has been detected.
         self.updates_watchdog_task = None
         self.updates_watchdog_event = asyncio.Event()
-        self.last_update_time = datetime.now()
-        self.listeners = {listener_type: [] for listener_type in pyrogram.enums.ListenerTypes}
+        self.last_update_time = datetime.now()}
 
         self.loop = asyncio.get_event_loop()
 
