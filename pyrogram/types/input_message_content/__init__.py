2c0381c96a9a0e928136fba6382feed29939a432
OnlyZalll <faizal.fajartn@gmail.com>
2025-05-23 17:54:43 +0700

    tes

--- pyrogram/types/input_message_content/__init__.py
+++ pyrogram/types/input_message_content/__init__.py
@@ -20,10 +20,12 @@ from .input_message_content import InputMessageContent
 from .input_reply_to_message import InputReplyToMessage
 from .input_reply_to_story import InputReplyToStory
 from .input_text_message_content import InputTextMessageContent
+from .text_quote import TextQuote
 
 __all__ = [
     "InputMessageContent",
     "InputReplyToMessage",
     "InputReplyToStory",
     "InputTextMessageContent",
+    "TextQuote",
 ]
